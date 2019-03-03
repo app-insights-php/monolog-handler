41fd37eeca4c98aca1da9e2e57c00c150c418eb1
Norbert Orzechowicz <norbert@orzechowicz.pl>
2019-03-03 15:20:30 +0100

    Added app insights dependency handler (#2)
    * Added app insights dependency handler
    
    * fixed failing tests

---
+++ src/AppInsightsPHP/Monolog/Handler/AppInsightsDependencyHandler.php
@@ -0,0 +1,61 @@
+<?php
+
+declare (strict_types=1);
+
+namespace AppInsightsPHP\Monolog\Handler;
+
+use AppInsightsPHP\Client\Client;
+use AppInsightsPHP\Monolog\Formatter\ContextFlatterFormatter;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+final class AppInsightsDependencyHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var Client
+     */
+    private $telemetryClient;
+
+    public function __construct(Client $telemetryClient)
+    {
+        parent::__construct(Logger::DEBUG, false);
+        $this->telemetryClient = $telemetryClient;
+    }
+
+    protected function write(array $record)
+    {
+        $formattedRecord = $this->formatter->format($record);
+
+        $this->telemetryClient->trackDependency(
+            $formattedRecord["channel"],
+            'Monolog Dependency Handler',
+            $record['message'],
+            null,
+            null,
+            $record['level'] >= Logger::ERROR ? false : true,
+            null,
+            array_merge(
+                [
+                    'datetime' => ($record['datetime'] instanceof \DateTimeInterface) ? $record['datetime']->format('c') : $record['datetime'],
+                    'monolog_level' => $record['level_name'],
+                ],
+                $formattedRecord['context']
+            )
+        );
+    }
+
+    protected function getDefaultFormatter()
+    {
+        return new ContextFlatterFormatter();
+    }
+
+    public function reset()
+    {
+        $this->telemetryClient->flush();
+    }
+
+    public function close()
+    {
+        return $this->reset();
+    }
+}
